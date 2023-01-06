37188bfeed3a77ae4fa9f05b3d577bf05e2feae9
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2023-01-06 06:38:57 +0000

    fix: sizing of top.

--- components/sections/Home.jsx
+++ components/sections/Home.jsx
@@ -3,7 +3,8 @@ import {
     Box,
     Text,
     Button,
-    useMediaQuery
+    useMediaQuery,
+    Divider,
 } from '@chakra-ui/react';
 import { PlayCircleFill } from '@styled-icons/bootstrap/PlayCircleFill'
 
@@ -11,6 +12,7 @@ import { PlayCircleFill } from '@styled-icons/bootstrap/PlayCircleFill'
 function HomeSection() {
     const [ isMobile ] = useMediaQuery('(max-width: 730px)', {ssr: true})
     const [ max900 ] = useMediaQuery('(max-width: 900px)', {ssr: true})
+    const [ max700 ] = useMediaQuery('(max-width: 700px)', {ssr: true})
     return (
         <Box 
         pos={'relative'}
@@ -22,7 +24,7 @@ function HomeSection() {
         backgroundRepeat={'no-repeat'}
         className={'home-desktop'}
         color={'#fff'}>
-            <Flex w={max900 === true ? '90vw' : '50vw'} maxW={isMobile === true ? '100vw' : '650px'} position={'relative'} top={'130px'} left={max900 === true ? '5vw' : '110px'} direction={'column'} gap={'40px'}>
+            <Flex w={max900 === true ? '90vw' : '50vw'} maxW={isMobile === true ? '100vw' : '650px'} position={'relative'} top={'7rem'} left={max900 === true ? '5vw' : '110px'} direction={'column'} gap={'40px'}>
                 <Text fontSize={isMobile === true ? '3rem' : '4rem'} fontWeight={'bold'} lineHeight={'120%'}>Inclusive care for children with special needs</Text>
                 <Flex gap={'24px'}>
                     <Button variant={'unstyled'}
@@ -48,6 +50,12 @@ function HomeSection() {
                         color: 'rgba(255,255,255,0.9)'
                     }}><PlayCircleFill size={'28px'} /> Play Video</Button>
                 </Flex>
+            </Flex>
+            <Flex pos={'absolute'} bottom={'2.5rem'} w={'90vw'} ml={'5vw'} display={max700 === true ? 'none' : 'flex'}
+            justifyContent={'space-between'} alignItems={'center'} gap={'20px'}>
+                <Text fontSize={'0.9rem'} fontWeight={'medium'} w={'fit-content'} display='ruby'>230 children under our care</Text>
+                <Divider />
+                <Text fontSize={'0.9rem'} fontWeight={'medium'} display='ruby'>58 donations collected</Text>
             </Flex>
         </Box>
     )
